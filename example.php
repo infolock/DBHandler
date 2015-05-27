a5bb2e9752a2f8fdb79ae80fea4ca0974b174f73
Jonathon Hibbard <jhibbard@gmail.com>
2015-05-27 09:22:38 -0400

    Create example.php

---
+++ example.php
@@ -0,0 +1,18 @@
+<?php
+###
+# BASIC EXAMPLE
+###
+$dbh = YOUR_PRIMARY_NAMESPACE_HERE\Database\DBHandler::getInstance()->dbHandle;
+$stmt = $dbh->prepare( "SELECT * FROM `some_table`" );
+$stmt->execute();
+$rows = $stmt->fetchAll( \PDO::FETCH_ASSOC );
+
+echo var_export( $row, true );
+
+
+
+###
+# SUBCLASS EXAMPLE
+###
+
+namespace YOUR_PRIMARY_NAMESPACE_HERE\Database;
